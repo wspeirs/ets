6cd845de107698dd9c902291bf879933f8c7cd5e
William Speirs <wspeirs@datto.com>
2018-08-17 17:48:23 -0400

    Added unit tests for range_set

--- src/range_set.rs
+++ src/range_set.rs
@@ -38,14 +38,136 @@ impl RangeSet {
     pub fn contains(&self, num: usize) -> bool {
         let res = self.ranges.binary_search_by(|r| {
             if num < r.start {
-                return Ordering::Less;
+                return Ordering::Greater;
             } else if r.start <= num && num < r.end {
                 return Ordering::Equal;
             } else {
-                return Ordering::Greater;
+                return Ordering::Less;
             }
         });
 
         res.is_ok()
     }
+}
+
+
+mod test {
+    use range_set::{RangeSetBuilder};
+
+    #[test]
+    pub fn test_create_empty() {
+        let mut rsb = RangeSetBuilder::new();
+        let rs = rsb.build();
+    }
+
+    #[test]
+    pub fn test_add() {
+        let mut rsb = RangeSetBuilder::new();
+
+        rsb.add(7);
+
+        let rs = rsb.build();
+
+        assert!(!rs.contains(6));
+        assert!(rs.contains(7));
+        assert!(!rs.contains(8));
+    }
+
+    #[test]
+    pub fn test_add_separate() {
+        let mut rsb = RangeSetBuilder::new();
+
+        rsb.add(7);
+        rsb.add(9);
+
+        let rs = rsb.build();
+
+        assert!(!rs.contains(6));
+        assert!(rs.contains(7));
+        assert!(!rs.contains(8));
+        assert!(rs.contains(9));
+        assert!(!rs.contains(10));
+    }
+
+    #[test]
+    pub fn test_add_together() {
+        let mut rsb = RangeSetBuilder::new();
+
+        rsb.add(7);
+        rsb.add(8);
+
+        let rs = rsb.build();
+
+        assert!(!rs.contains(6));
+        assert!(rs.contains(7));
+        assert!(rs.contains(8));
+        assert!(!rs.contains(9));
+    }
+
+    #[test]
+    pub fn test_add_range() {
+        let mut rsb = RangeSetBuilder::new();
+
+        rsb.add_range(1..3);
+
+        let rs = rsb.build();
+
+        assert!(!rs.contains(0));
+        assert!(rs.contains(1));
+        assert!(rs.contains(2));
+        assert!(!rs.contains(3));
+    }
+
+    #[test]
+    pub fn test_add_range_separate() {
+        let mut rsb = RangeSetBuilder::new();
+
+        rsb.add_range(1..3);
+        rsb.add_range(4..6);
+
+        let rs = rsb.build();
+
+        assert!(!rs.contains(0));
+        assert!(rs.contains(1));
+        assert!(rs.contains(2));
+        assert!(!rs.contains(3));
+        assert!(rs.contains(4));
+        assert!(rs.contains(5));
+        assert!(!rs.contains(6));
+    }
+
+    #[test]
+    pub fn test_add_range_together() {
+        let mut rsb = RangeSetBuilder::new();
+
+        rsb.add_range(1..3);
+        rsb.add_range(3..5);
+
+        let rs = rsb.build();
+
+        assert!(!rs.contains(0));
+        assert!(rs.contains(1));
+        assert!(rs.contains(2));
+        assert!(rs.contains(3));
+        assert!(rs.contains(4));
+        assert!(!rs.contains(5));
+    }
+
+    #[test]
+    pub fn test_mixed() {
+        let mut rsb = RangeSetBuilder::new();
+
+        rsb.add_range(1..3);
+        rsb.add(4);
+
+        let rs = rsb.build();
+
+        assert!(!rs.contains(0));
+        assert!(rs.contains(1));
+        assert!(rs.contains(2));
+        assert!(!rs.contains(3));
+        assert!(rs.contains(4));
+        assert!(!rs.contains(5));
+    }
+
 }
